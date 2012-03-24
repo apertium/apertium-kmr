7d058ad4afa2ee2c24f1ce992f7d2f5bd25f480c
Francis M. Tyers <ftyers@prompsit.com>
2012-03-24 19:34:54 +0000

    more

---
+++ dev/conj.sh
@@ -0,0 +1,6 @@
+echo -n "" > /tmp/v
+for i in `cat test-words.txt | sed 's/\t/,/g'`; do 
+	echo $i | cut -f2 -d',' | hfst-proc ../kur.automorf.hfst | sed 's/$/\t/g' | cut -f2- | sh ~/scripts/remove-newline.sh >> /tmp/v
+done
+
+paste test-words.txt /tmp/v
